adf5874e28c4357070763211b325e1fb984deff4
Hwangseunghyun <ghkdsigm3@gmail.com>
2022-12-27 11:12:47 +0900

    computed 추가

--- my-project/src/pages/Sub12.vue
+++ my-project/src/pages/Sub12.vue
@@ -2,8 +2,17 @@
 <template>
   <div>
     <input v-model="question" />
+    <input v-model="question2" />
     <p>{{ answer }}</p>
-    <p>{{ changeAnswer }}</p>
+    <p>{{ answer2 }}</p>
+    <div>
+      <h1>Bankrruped</h1>
+      <ul>
+        <li v-for="(item, index) in bankrrupedPeople" :key="index">
+          {{item.name}}
+        </li>
+      </ul>
+    </div>
   </div>  
 </template>
 
@@ -13,7 +22,13 @@
       return {
         question: '',
         answer: 'watch에 의한 변경값',
-        answer2: 'counputed에 의한 변경값'
+        answer2: 'counputed에 의한 변경값',
+        accounts: [
+            { name: 'neo', balance: 500, isBankrruped: true },
+            { name: 'tak', balance: 700, isBankrruped: false },
+            { name: 'john', balance: 350, isBankrruped: false },
+            { name: 'justin', balance: 200, isBankrruped: true },
+        ],
       }
     },
     watch: {
@@ -23,10 +38,22 @@
       }
     },
     computed:{
-      changeAnswer(){
-        // eslint-disable-next-line vue/no-side-effects-in-computed-properties
-        return this.answer2 = this.question
-      } 
+      question2:{
+        // get: function() {
+        //   // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+        //   return this.answer2 = 'counputed에 의한 변경값'
+        // },
+        set: function(data) {
+          // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+          return this.answer2 = data
+        }
+      },
+      bankrrupedPeople: function () {
+          const newArr = this.accounts.filter((account) => {
+              return account.isBankrruped
+          })
+          return newArr
+      }    
     },
     methods:{
       getAnswer(){
