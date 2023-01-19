82bf089d13b516fc0e6db51a8344ea9e647fc80a
Hwangseunghyun <ghkdsigm3@gmail.com>
2023-01-19 16:09:39 +0900

    홈 화면

--- my-project/src/pages/Home.vue
+++ my-project/src/pages/Home.vue
@@ -1,10 +1,143 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div>home</div>
+  <div>
+    <table class="lisTable">
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Contents</th>
+          <th>Link</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in vueData" :key="index">
+          <td>{{index + 1}}</td>
+          <td>{{item.content}}</td>
+          <td>
+            <router-link :to="item.link">바로가기</router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data(){
+    return {
+      vueData: [
+        {
+          content:'슬롯1',
+          link:'sub01'
+        },
+        {
+          content:'Prop',
+          link:'sub02'
+        },
+        {
+          content:'Emit',
+          link:'sub03'
+        },
+        {
+          content:'슬롯2',
+          link:'sub04'
+        },
+        {
+          content:'탭',
+          link:'sub05'
+        },
+        {
+          content:'게임셀렉트',
+          link:'sub06'
+        },
+        {
+          content:'유효성',
+          link:'sub07'
+        },
+        {
+          content:'API',
+          link:'sub08'
+        },
+        {
+          content:'VUETIFYTABLE01',
+          link:'sub09'
+        },
+        {
+          content:'VUETIFYTABLE02',
+          link:'sub10'
+        },
+        {
+          content:'VUETIFYTABLE03',
+          link:'sub11'
+        },
+        {
+          content:'VUETIFYTABLE04',
+          link:'sub19'
+        },
+        {
+          content:'Computed & Watch',
+          link:'sub12'
+        },
+        {
+          content:'Computed',
+          link:'sub13'
+        },
+        {
+          content:'Scoped slots',
+          link:'sub14'
+        },
+        {
+          content:'Named slots',
+          link:'sub15'
+        },
+        {
+          content:'Bay API',
+          link:'sub16'
+        },
+        {
+          content:'PARAMS',
+          link:'sub17'
+        },
+        {
+          content:'쿠키/로컬스토리지',
+          link:'sub18'
+        },
+        {
+          content:'파일첨부',
+          link:'sub19'
+        },
+        {
+          content:'MIXIN',
+          link:'sub20'
+        },
+        {
+          content:'CKEditor',
+          link:'sub21'
+        },
+        {
+          content:'Vee-validator',
+          link:'sub22'
+        },
+        {
+          content:'슬롯',
+          link:'sub23'
+        },
+        {
+          content:'슬롯',
+          link:'sub24'
+        }
+      ]
+    }
+  }
+};
 </script>
 
-<style></style>
+<style scoped>
+.lisTable {width:50%; left:50%; transform:translateX(50%);}
+.lisTable thead tr th {font-weight:500;font-size:12px;font-weight:bold;}
+.lisTable tbody tr td:nth-of-type(1) {font-weight:300;}
+.lisTable tbody tr td {font-weight:500;font-size:12px;}
+.lisTable tr:hover {background:#eee;}
+a{ color:rgb(71, 71, 131) !important}
+</style>
