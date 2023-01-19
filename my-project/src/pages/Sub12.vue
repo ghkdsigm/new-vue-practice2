ecd464281a7cb7f6b2b890e7f000d1845258a2ac
ghkdsigm3 <ghkdsigm3@gmail.com>
2023-01-19 19:24:15 +0900

    sub12

--- my-project/src/pages/Sub12.vue
+++ my-project/src/pages/Sub12.vue
@@ -9,16 +9,14 @@
       <h1>Bankrruped</h1>
       <ul>
         <li v-for="(item, index) in bankrrupedPeople" :key="index">
-          {{item.name}}
+          {{ item.name }}
         </li>
-      </ul>      
+      </ul>
     </div>
-    <div>      
+    <div>
       <div v-for="person in activePeople" :key="person.name">
-            {{ person.name }} : {{ person.email }}  
-            <div :class="{ active: person.isActive }">
-              This is the menu option
-            </div>
+        {{ person.name }} : {{ person.email }}
+        <div :class="{ active: person.isActive }">This is the menu option</div>
       </div>
     </div>
     <div>
@@ -26,82 +24,84 @@
       <Sub v-else />
       <button @click="changeLayout">레이아웃 변경</button>
     </div>
-  </div>  
+  </div>
 </template>
 
-<script>  
-  import Default from '@/components/layout/Default'
-  import Sub from '@/components/layout/Sub'
+<script>
+import Default from '@/components/layout/Default';
+import Sub from '@/components/layout/Sub';
 
-  export default {
-    data(){
-      return {
-        question: '',
-        answer: 'watch에 의한 변경값',
-        answer2: '',
-        accounts: [
-            { name: 'neo', balance: 500, isBankrruped: true },
-            { name: 'tak', balance: 700, isBankrruped: false },
-            { name: 'john', balance: 350, isBankrruped: false },
-            { name: 'justin', balance: 200, isBankrruped: true },
-        ],
-        classObject: {
-          active: true,
-          'text-danger': false
-        },
-        people: [
-            { name: 'Mike Taylor', email: 'mike@example.com', isActive: true},
-            { name: 'John Carson', email: 'john@example.com', isActive: false},
-            { name: 'Peter Sanders', email: 'peter@exemple.com', isActive: true}
-        ],
-        layout: { name:'dafault', isActive: false}
-      }
-    },
-    components:{
-      Default,
-      Sub
-    },
-    watch: {
-      question: function(newQuestion){
-        if(newQuestion !== '') this.getAnswer();
-        else this.answer = '선 질문 후 대답'        
-      }
-    },
-    computed:{
-      question2:{
-        get: function() {
-          // eslint-disable-next-line vue/no-side-effects-in-computed-properties
-          return this.answer2
-        },
-        set: function(data) {
-          // eslint-disable-next-line vue/no-side-effects-in-computed-properties
-          return this.answer2 = data
-        }
+export default {
+  data() {
+    return {
+      question: '',
+      answer: 'watch에 의한 변경값',
+      answer2: '',
+      accounts: [
+        { name: 'neo', balance: 500, isBankrruped: true },
+        { name: 'tak', balance: 700, isBankrruped: false },
+        { name: 'john', balance: 350, isBankrruped: false },
+        { name: 'justin', balance: 200, isBankrruped: true },
+      ],
+      classObject: {
+        active: true,
+        'text-danger': false,
       },
-      bankrrupedPeople: function () {
-          const newArr = this.accounts.filter((account) => {
-              return account.isBankrruped
-          })
-          return newArr
-      },
-      activePeople() {
-          return this.people.filter((person) => person.isActive)
-      },
-      activeLayout(){
-        return  this.layout.isActive === true        
-      }       
+      people: [
+        { name: 'Mike Taylor', email: 'mike@example.com', isActive: true },
+        { name: 'John Carson', email: 'john@example.com', isActive: false },
+        { name: 'Peter Sanders', email: 'peter@exemple.com', isActive: true },
+      ],
+      layout: { name: 'dafault', isActive: false },
+    };
+  },
+  components: {
+    Default,
+    Sub,
+  },
+  watch: {
+    question: function (newQuestion) {
+      if (newQuestion !== '') this.getAnswer();
+      else this.answer = '선 질문 후 대답';
     },
-    methods:{
-      getAnswer(){
-        this.answer = this.question
+  },
+  computed: {
+    question2: {
+      get: function () {
+        // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+        return this.answer2;
       },
-      changeLayout(){
-        this.layout.isActive = !this.layout.isActive
-      }
-    }
-  }
+      set: function (data) {
+        // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+        return (this.answer2 = data);
+      },
+    },
+    bankrrupedPeople: function () {
+      const newArr = this.accounts.filter((account) => {
+        return account.isBankrruped;
+      });
+      return newArr;
+    },
+    activePeople() {
+      return this.people.filter((person) => person.isActive);
+    },
+    activeLayout() {
+      return this.layout.isActive === true;
+    },
+  },
+  methods: {
+    getAnswer() {
+      this.answer = this.question;
+    },
+    changeLayout() {
+      this.layout.isActive = !this.layout.isActive;
+    },
+  },
+};
 </script>
 
 <style scoped>
-input {border:1px solid #ccc;}
+input {
+  border: 1px solid #ccc;
+}
 </style>
