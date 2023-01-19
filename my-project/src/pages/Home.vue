9565320889dea22524497ebcb6084ae40858de95
ghkdsigm3 <ghkdsigm3@gmail.com>
2023-01-19 19:27:25 +0900

    홈 위치수정

--- my-project/src/pages/Home.vue
+++ my-project/src/pages/Home.vue
@@ -11,8 +11,8 @@
       </thead>
       <tbody>
         <tr v-for="(item, index) in vueData" :key="index">
-          <td>{{index + 1}}</td>
-          <td>{{item.content}}</td>
+          <td>{{ index + 1 }}</td>
+          <td>{{ item.content }}</td>
           <td>
             <router-link :to="item.link">바로가기</router-link>
           </td>
@@ -24,120 +24,130 @@
 
 <script>
 export default {
-  data(){
+  data() {
     return {
       vueData: [
         {
-          content:'슬롯1',
-          link:'sub01'
+          content: '슬롯1',
+          link: 'sub01',
         },
         {
-          content:'Prop',
-          link:'sub02'
+          content: 'Prop',
+          link: 'sub02',
         },
         {
-          content:'Emit',
-          link:'sub03'
+          content: 'Emit',
+          link: 'sub03',
         },
         {
-          content:'슬롯2',
-          link:'sub04'
+          content: '슬롯2',
+          link: 'sub04',
         },
         {
-          content:'탭',
-          link:'sub05'
+          content: '탭',
+          link: 'sub05',
         },
         {
-          content:'게임셀렉트',
-          link:'sub06'
+          content: '게임셀렉트',
+          link: 'sub06',
         },
         {
-          content:'유효성',
-          link:'sub07'
+          content: '유효성',
+          link: 'sub07',
         },
         {
-          content:'API',
-          link:'sub08'
+          content: 'API',
+          link: 'sub08',
         },
         {
-          content:'VUETIFYTABLE01',
-          link:'sub09'
+          content: 'VUETIFYTABLE01',
+          link: 'sub09',
         },
         {
-          content:'VUETIFYTABLE02',
-          link:'sub10'
+          content: 'VUETIFYTABLE02',
+          link: 'sub10',
         },
         {
-          content:'VUETIFYTABLE03',
-          link:'sub11'
+          content: 'VUETIFYTABLE03',
+          link: 'sub11',
         },
         {
-          content:'VUETIFYTABLE04',
-          link:'sub19'
+          content: 'VUETIFYTABLE04',
+          link: 'sub19',
         },
         {
-          content:'Computed & Watch',
-          link:'sub12'
+          content: 'Computed & Watch',
+          link: 'sub12',
         },
         {
-          content:'Computed',
-          link:'sub13'
+          content: 'Computed',
+          link: 'sub13',
         },
         {
-          content:'Scoped slots',
-          link:'sub14'
+          content: 'Scoped slots',
+          link: 'sub14',
         },
         {
-          content:'Named slots',
-          link:'sub15'
+          content: 'Named slots',
+          link: 'sub15',
         },
         {
-          content:'Bay API',
-          link:'sub16'
+          content: 'Bay API',
+          link: 'sub16',
         },
         {
-          content:'PARAMS',
-          link:'sub17'
+          content: 'PARAMS',
+          link: 'sub17',
         },
         {
-          content:'쿠키/로컬스토리지',
-          link:'sub18'
+          content: '쿠키/로컬스토리지',
+          link: 'sub20',
         },
         {
-          content:'파일첨부',
-          link:'sub19'
+          content: '파일첨부',
+          link: 'sub21',
         },
         {
-          content:'MIXIN',
-          link:'sub20'
+          content: 'MIXIN',
+          link: 'sub22',
         },
         {
-          content:'CKEditor',
-          link:'sub21'
+          content: 'CKEditor',
+          link: 'sub23',
         },
         {
-          content:'Vee-validator',
-          link:'sub22'
+          content: 'Vee-validator',
+          link: 'sub24',
         },
-        {
-          content:'슬롯',
-          link:'sub23'
-        },
-        {
-          content:'슬롯',
-          link:'sub24'
-        }
-      ]
-    }
-  }
+      ],
+    };
+  },
 };
 </script>
 
 <style scoped>
-.lisTable {width:50%; left:50%; transform:translateX(50%); margin-bottom:15%;}
-.lisTable thead tr th {font-weight:500;font-size:12px;font-weight:bold;}
-.lisTable tbody tr td:nth-of-type(1) {font-weight:300;}
-.lisTable tbody tr td {font-weight:500;font-size:12px;}
-.lisTable tr:hover {background:#eee;}
-a{ color:rgb(71, 71, 131) !important}
+.lisTable {
+  width: 50%;
+  left: 50%;
+  transform: translateX(50%);
+  margin-bottom: 15%;
+}
+.lisTable thead tr th {
+  font-weight: 500;
+  font-size: 12px;
+  font-weight: bold;
+}
+.lisTable tbody tr td:nth-of-type(1) {
+  font-weight: 300;
+}
+.lisTable tbody tr td {
+  font-weight: 500;
+  font-size: 12px;
+}
+.lisTable tr:hover {
+  background: #eee;
+}
+a {
+  color: rgb(71, 71, 131) !important;
+}
 </style>
