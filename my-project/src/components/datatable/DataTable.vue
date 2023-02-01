c7e6f6dea58c26256681eb5c38af52fab3453751
ghkdsigm3 <ghkdsigm3@gmail.com>
2023-02-01 22:22:49 +0900

    dd

--- my-project/src/components/datatable/DataTable.vue
+++ my-project/src/components/datatable/DataTable.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <v-switch v-model="allowDrag" label="Draggable">
-    </v-switch>
+    <v-switch v-model="allowDrag" label="Draggable"> </v-switch>
     <v-data-table
       ref="myTable"
       v-model="selected"
@@ -10,9 +9,9 @@
       item-key="name"
       class="elevation-1"
     >
-      <template #body="kk">
+      <template #body="props">
         <draggable
-          :list="kk.items"
+          :list="props.items"
           tag="tbody"
           :disabled="!allowDrag"
           :move="onMoveCallback"
@@ -20,15 +19,15 @@
           @end="onDropCallback"
         >
           <DataTableRowHandler
-            v-for="(item, index) in kk.items"
+            v-for="(item, index) in props.items"
             :key="index"
             :item="item"
             :headers="activeHeaders"
             :item-class="getClass(item)"
           >
-            <template #item.locked="{ item }">
+            <template #item.lock="{ item }">
               <v-icon @click="item.locked = item.locked ? false : true">{{
-                item.locked ? "mdi-pin-outline" : "mdi-pin-off-outline"
+                item.locked ? 'mdi-pin-outline' : 'mdi-pin-off-outline'
               }}</v-icon>
             </template>
 
@@ -36,10 +35,10 @@
               {{ item.carbs }}
               <v-icon>{{
                 item.carbs > 80
-                  ? "mdi-speedometer"
+                  ? 'mdi-speedometer'
                   : item.carbs > 45
-                  ? "mdi-speedometer-medium"
-                  : "mdi-speedometer-slow"
+                  ? 'mdi-speedometer-medium'
+                  : 'mdi-speedometer-slow'
               }}</v-icon>
             </template>
           </DataTableRowHandler>
@@ -50,8 +49,8 @@
 </template>
 
 <script>
-import DataTableRowHandler from "@/components/datatable/DataTableRowHandler.vue";
-import draggable from "vuedraggable";
+import DataTableRowHandler from '@/components/datatable/DataTableRowHandler.vue';
+import draggable from 'vuedraggable';
 
 export default {
   components: {
@@ -63,108 +62,108 @@ export default {
       allowDrag: true,
       selected: [],
       headers: [
-        { text: "Lock", value: "lock", width: "50px", sortable: false },
+        { text: 'Lock', value: 'lock', width: '50px', sortable: false },
         {
-          text: "Dessert (100g serving)",
-          align: "start",
+          text: 'Dessert (100g serving)',
+          align: 'start',
           sortable: false,
-          value: "name",
+          value: 'name',
         },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" },
+        { text: 'Calories', value: 'calories' },
+        { text: 'Fat (g)', value: 'fat' },
+        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Iron (%)', value: 'iron' },
       ],
       desserts: [
         {
-          name: "Frozen Yogurt",
+          name: 'Frozen Yogurt',
           calories: 159,
           fat: 6.0,
           carbs: 24,
           protein: 4.0,
-          iron: "1%",
+          iron: '1%',
           locked: false,
         },
         {
-          name: "Ice cream sandwich",
+          name: 'Ice cream sandwich',
           calories: 237,
           fat: 9.0,
           carbs: 37,
           protein: 4.3,
-          iron: "1%",
+          iron: '1%',
           locked: true,
         },
         {
-          name: "Eclair",
+          name: 'Eclair',
           calories: 262,
           fat: 16.0,
           carbs: 23,
           protein: 6.0,
-          iron: "7%",
+          iron: '7%',
           locked: false,
         },
         {
-          name: "Cupcake",
+          name: 'Cupcake',
           calories: 305,
           fat: 3.7,
           carbs: 67,
           protein: 4.3,
-          iron: "8%",
+          iron: '8%',
           locked: false,
         },
         {
-          name: "Gingerbread",
+          name: 'Gingerbread',
           calories: 356,
           fat: 16.0,
           carbs: 49,
           protein: 3.9,
-          iron: "16%",
+          iron: '16%',
           locked: false,
         },
         {
-          name: "Jelly bean",
+          name: 'Jelly bean',
           calories: 375,
           fat: 0.0,
           carbs: 94,
           protein: 0.0,
-          iron: "0%",
+          iron: '0%',
           locked: false,
         },
         {
-          name: "Lollipop",
+          name: 'Lollipop',
           calories: 392,
           fat: 0.2,
           carbs: 98,
           protein: 0,
-          iron: "2%",
+          iron: '2%',
           locked: false,
         },
         {
-          name: "Honeycomb",
+          name: 'Honeycomb',
           calories: 408,
           fat: 3.2,
           carbs: 87,
           protein: 6.5,
-          iron: "45%",
+          iron: '45%',
           locked: false,
         },
         {
-          name: "Donut",
+          name: 'Donut',
           calories: 452,
           fat: 25.0,
           carbs: 51,
           protein: 4.9,
-          iron: "22%",
+          iron: '22%',
           locked: true,
         },
         {
-          name: "KitKat",
+          name: 'KitKat',
           calories: 518,
           fat: 26.0,
           carbs: 65,
           protein: 7,
-          iron: "6%",
+          iron: '6%',
           locked: false,
         },
       ],
@@ -173,7 +172,7 @@ export default {
   computed: {
     activeHeaders() {
       return this.headers.filter((h) => {
-        if (!this.allowDrag && h.value === "lock") {
+        if (!this.allowDrag && h.value === 'lock') {
           return false;
         }
         return true;
@@ -194,10 +193,10 @@ export default {
     // },
     getClass(item) {
       return item.calories > 500
-        ? "cal-high"
+        ? 'cal-high'
         : item.calories > 400
-        ? "cal-medium"
-        : "cal-low";
+        ? 'cal-medium'
+        : 'cal-low';
     },
     onCloneCallback(item) {
       // Create a fresh copy of item
@@ -209,7 +208,7 @@ export default {
       const item = evt.draggedContext.element;
       const itemIdx = evt.draggedContext.futureIndex;
 
-      console.log("onMoveCallback");
+      console.log('onMoveCallback');
 
       if (item.locked) {
         return false;
@@ -218,7 +217,7 @@ export default {
       return true;
     },
     onDropCallback(evt, originalEvent) {
-      console.log("onDropCallback");
+      console.log('onDropCallback');
     },
   },
 };
