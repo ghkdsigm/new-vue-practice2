3b53ebcc2b9101c97bdee809bd17e37e12bafe43
ghkdsigm3 <ghkdsigm3@gmail.com>
2023-08-19 20:24:11 +0900

    url update

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1014,6 +1014,30 @@ export default {
           series: 'y',
           type: 'cs',
         },
+        {
+          title: 'JavaScript 정규 표현식 문법 총정리 + 응용 예제',
+          url: 'https://inpa.tistory.com/entry/JS-%F0%9F%93%9A-%EC%A0%95%EA%B7%9C%EC%8B%9D-RegExp-%EB%88%84%EA%B5%AC%EB%82%98-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0-%EC%89%BD%EA%B2%8C-%EC%A0%95%EB%A6%AC',
+          series: 'y',
+          type: 'cs',
+        },
+        {
+          title: '',
+          url: '',
+          series: 'y',
+          type: 'cs',
+        },
+        {
+          title: '',
+          url: '',
+          series: 'y',
+          type: 'cs',
+        },
+        {
+          title: '',
+          url: '',
+          series: 'y',
+          type: 'cs',
+        },
       ],
       listempty: false,
     };
