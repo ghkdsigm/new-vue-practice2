97c0a93defd4f439e470b8702aab5c8dc249b61e
ghkdsigm3 <ghkdsigm3@gmail.com>
2023-09-03 19:13:08 +0900

    url update

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1174,10 +1174,10 @@ export default {
           type: 'frontend',
         },
         {
-          title: '',
-          url: '',
+          title: '웹페이지 렌더링을 7배 빠르게 하는 방법: content-visibility',
+          url: 'https://namunamu1105.medium.com/%EC%9B%B9%ED%8E%98%EC%9D%B4%EC%A7%80-%EB%A0%8C%EB%8D%94%EB%A7%81%EC%9D%84-7%EB%B0%B0-%EB%B9%A0%EB%A5%B4%EA%B2%8C-%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95-content-visibility-1a23d3f7ed9',
           series: '',
-          type: '',
+          type: 'frontend',
         },
         {
           title: '',
